f754f28bfa51fb3a538e1397903914157dee3730
Thomas Callum Brook <thomascallumbrook@gmail.com>
2018-06-21 11:27:49 +0100

    Complete Learn Enough To Be Dangerous JS tutorial

--- bundle.js
+++ bundle.js
@@ -43,21 +43,23 @@ function Phrase(content) {
 
 },{}],2:[function(require,module,exports){
 let Phrase = require("tcbrook-palindrome");
-function palindromeTester() {
-  let string = prompt("Please enter a string for palindrome testing:");
-  let phrase = new Phrase(string);
+function palindromeTester(event) {
+  event.preventDefault();
+
+  let phrase = new Phrase(event.target.phrase.value);
   let palindromeResult = document.querySelector("#palindromeResult");
 
   if (phrase.palindrome()) {
-    palindromeResult.innerHTML = `<strong>${phrase.content}</strong> is a palindrome!`;
+    palindromeResult.innerHTML = `<strong>"${phrase.content}"</strong> is a palindrome!`;
   } else {
-    palindromeResult.innerHTML = `<strong>${phrase.content}</strong> is a not a palindrome.`;
+    palindromeResult.innerHTML = `<strong>"${phrase.content}"</strong> is a not a palindrome.`;
   }
 }
+
 document.addEventListener("DOMContentLoaded", function() {
-  let button = document.querySelector("#palindromeTester");
-  button.addEventListener("click", function() {
-    palindromeTester();
+  let tester = document.querySelector("#palindromeTester");
+  tester.addEventListener("submit", function(event) {
+    palindromeTester(event);
   });
 });
 
